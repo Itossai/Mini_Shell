1714476e1f7ecfe8b94452f3fb3a41ca3f1912af
Itossai <62126283+Itossai@users.noreply.github.com>
2022-09-30 20:00:14 -0300

    Shell aplicado com sucesso.

---
+++ mini_shell.c
@@ -0,0 +1,39 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h> 
+#define TRUE 1
+#define MAX 200
+void command(char command[MAX],char *argumento[20]){
+  argumento[0]=strtok(command,"\0");
+  for (int i=0;i<20;i++){
+    strtok(NULL,"\0");
+  }
+}
+
+
+int main(){
+  int status;
+  char promt[MAX];
+  char *argumento[20];
+  while(TRUE){
+    printf("\nDigite o comando a ser soliciatado:      \t");
+    if (fgets(promt, strlen(promt), stdin) ==           NULL) {
+      printf("error");
+      }else{
+        promt[strcspn(promt, "\n")] = 0;
+        command(promt,argumento);
+        if (fork()!=0){
+          waitpid(-1,&status,0);
+          continue;
+        }else{
+          execve(argumento[0], argumento, 0);
+        }
+      
+      }
+    }
+}
+
